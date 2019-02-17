1c9803879164285a71030f7f3126cc0ae1d0263f
srozb <github@rozbicki.eu>
2019-02-17 12:24:48 +0100

    dht22 sensor rewrite

--- sensors/dht22/sensor.py
+++ sensors/dht22/sensor.py
@@ -1,39 +1,19 @@
 #!/usr/bin/env python3
 
 import Adafruit_DHT
-import paho.mqtt.publish as publish
-import time
-import json
 import logging
-import config
 
 
-def read_data():
-    return Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+class Sensor():
+    def __init__(self):
+        ""
 
+    def setup(self, params):
+        self.PORT = params['port']
 
-def send_data(h, t):
-    auth = {'username': config.LOGIN, 'password': config.PASSWORD}
-    payload = json.dumps({'temperature': round(t, 1), 'humidity': round(h)})
-    logging.info(payload)
-    publish.single(config.TOPIC, payload, hostname=config.HOSTNAME, auth=auth)
-
-
-def main():
-    logging.info("dht2mqtt started")
-    while True:
-        h, t = read_data()
-        if h > 0 and h < 100 and t > 0 and t < 100:
-            try:
-                send_data(h, t)
-            except OSError as err:
-                logging.error(
-                    "couldn't contact mqtt server. Giving up till the next measurement. {}"
-                    .format(err))
-        else:
-            logging.warning("missing or incorrect data from sensor.")
-            logging.warning("t: {}, h: {}".format(t, h))
-        time.sleep(config.INTERVAL)
-
-
-main()
+    def read(self):
+        logging.debug("read")
+        h, t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, self.PORT)
+        readings = {'temperature': round(t, 1), 'humidity': round(h)}
+        logging.debug(readings)
+        return readings
